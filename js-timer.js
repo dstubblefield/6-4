fcc3c181b33e9cf2575f9f2180e1db3f944f98ad
Andrew Stubblefield <drew@dstubblefield.com>
2023-02-04 18:20:47 -0600

    Final Count Down

--- js-timer.js
+++ js-timer.js
@@ -1,26 +1,26 @@
-function countDown(count) {
-  const counts = setInterval(function () {
-    count--;
-    if (count <= 0) {
-      clearInterval(counts);
-      console.log("Done");
+function theFinalCountDown(seconds) {
+  let countDown = setInterval(() => {
+    seconds--;
+    if (seconds <= 0) {
+      clearInterval(countDown);
+      console.log("DONE!");
     } else {
-      console.log(count);
+      console.log(seconds);
     }
-  }, 1000);
+  });
 }
 
-function randomGame() {
-  let number;
-  let counter = 0;
-  let draw = setInterval(function () {
-    number = Math.random();
-    counter++;
-    if (number > 0.75) {
-      clearInterval(draw);
-      console.log(`Number of tries: ${counter}`);
-    } else {
-      console.log(`Number: ${number}, Tries: ${counter}`);
-    }
-  }, 1000);
-}
+// function randomGame() {
+//   let number;
+//   let counter = 0;
+//   let draw = setInterval(function () {
+//     number = Math.random();
+//     counter++;
+//     if (number > 0.75) {
+//       clearInterval(draw);
+//       console.log(`Number of tries: ${counter}`);
+//     } else {
+//       console.log(`Number: ${number}, Tries: ${counter}`);
+//     }
+//   }, 1000);
+// }
